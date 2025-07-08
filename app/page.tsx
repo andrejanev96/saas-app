49c2afb925668662ebd66470c592712c8d2b8d99
andrejanev96 <>
2025-07-08 17:45:21 +0200

    add companion cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,43 @@
-import {Button} from "@/components/ui/button"
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className = "text-2xl underline">Welcome to my SaaS app!</h1>
-      <Button>
-          Let's get started!
-      </Button>
-    </div>
+    <main>
+      <h1 className = "text-2xl underline">Popular Companions</h1>
+        <section className = 'home-section'>
+            <CompanionCard
+            id = '123'
+            name="Neura the Brainy Explorer"
+            topic="Neural Network of the Brain"
+            subject="science"
+            duration={30}
+            color="#ffda6e"
+            />
+            <CompanionCard
+                id = '456'
+                name="Countsy the Number Wizard"
+                topic="Derivatives & Integrals"
+                subject="maths"
+                duration={30}
+                color="#e5d0ff"
+            />
+            <CompanionCard
+                id = '789'
+                name="Verba the Vocabulary Builder"
+                topic="Language"
+                subject="English Literature"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+        <section className = 'home-section'>
+            <CompanionsList/>
+            <CTA/>
+        </section>
+
+    </main>
   )
 }
 
---
+++ components/CTA.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CTA = () => {
+    return (
+        <div>CTA</div>
+    )
+}
+export default CTA
